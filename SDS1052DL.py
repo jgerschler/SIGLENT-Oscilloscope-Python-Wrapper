5488b55fc7ff859c8d9254227410e44a470f2446
jgerschler <jjgerschler@gmail.com>
2017-01-15 14:56:55 -0600

    in progress -- not useful yet

--- SDS1052DL.py
+++ SDS1052DL.py
@@ -3,9 +3,14 @@
 import visa, time
 
 class Oscilloscope(object):
-    def __init__(self):
+    #def __init__(self):
 
     def find_oscopes(self):
-        self.rm = visa.ResourceManager()
+        rm = visa.ResourceManager()
+        siglent_list = [instrument for instrument in rm.list_resources() if instrument.split('::')[1] == '0xF4EC']
+        for instrument in siglent_list:
+            print('Found a Siglent Oscilloscope. Serial:{}'.format(instrument.split('::')[3]))
+                
+            
         
         
