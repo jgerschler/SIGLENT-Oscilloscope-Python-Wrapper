ff2f6fc40dfbcc3b2fc0213511e8b79a734cbf2d
jgerschler <jjgerschler@gmail.com>
2017-01-15 17:49:29 -0600

    in progress -- not useful yet

--- SDS1052DL.py
+++ SDS1052DL.py
@@ -5,12 +5,23 @@ import visa, time
 class Oscilloscope(object):
     #def __init__(self):
 
-    def enumerate_osc(self):
-        rm = visa.ResourceManager()
-        siglent_list = [instrument for instrument in rm.list_resources() if instrument.split('::')[1] == '0xF4EC']
-        for idx, instrument in enumerate(siglent_list):
-            print('ID:{0} (Oscilloscope Serial No. {1})'.format(str(idx),instrument.split('::')[3]))
-                
+    def connect(self, instrument_id=0):
+        self.rm = visa.ResourceManager()
+        siglent_list = [instrument for instrument in self.rm.list_resources() if instrument.split('::')[1] == '0xF4EC']
+        if len(siglent_list) >= instrument_id+1:
+            print("Available oscilloscopes:")
+            for idx, instrument in enumerate(siglent_list):
+                print("ID:{0} (Oscilloscope Serial No. {1})".format(str(idx),instrument.split('::')[3]))
+            print("")
+            print("Connecting to oscilloscope (ID {}).".format(str(instrument_id)))
+            osc = self.rm.open_resource(siglent_list[instrument_id])
+            print("")
+            print("Connected.")
+            return
+        else:
+            print("No Siglent oscilloscopes found, or you've selected a nonexistent oscilloscope ID")
+            return
             
-        
+    def disconnect(self):
+        self.rm.close()
         
