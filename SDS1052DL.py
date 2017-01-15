3ff4ff37c5daf45de1af4a49a8891caf85806667
jgerschler <jjgerschler@gmail.com>
2017-01-15 14:38:41 -0600

    in progress -- not useful yet

---
+++ SDS1052DL.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# This is VERY much still in progress. Not ready to be used!
+import visa, time
+
+class Oscilloscope(object):
+    def __init__(self):
+
+    def find_oscopes(self):
+        self.rm = visa.ResourceManager()
+        
+        
