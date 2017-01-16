0c49217c3b70cde74de3eaf2167d927a99610c90
jgerschler <jjgerschler@gmail.com>
2017-01-16 16:50:52 -0600

    being updated

--- SDS1052DL.py
+++ SDS1052DL.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# This is VERY much still in progress. Not ready to be used!
 import visa, time
 
 class Oscilloscope(object):
@@ -10,15 +9,18 @@ class Oscilloscope(object):
         self.rm = visa.ResourceManager()
         siglent_list = [instrument for instrument in self.rm.list_resources() if instrument.split('::')[1] == '0xF4EC']
         if len(siglent_list) >= instrument_id+1:
-            print("Available oscilloscopes:")
-            for idx, instrument in enumerate(siglent_list):
-                print("ID:{0} (Oscilloscope Serial No. {1})".format(str(idx),instrument.split('::')[3]))
-            print("")
-            print("Connecting to oscilloscope (ID {0}).".format(str(instrument_id)))
-            self.osc = self.rm.open_resource(siglent_list[instrument_id])
-            print("")
-            print("Connected.")
-            return
+            try:
+                print("Available oscilloscopes:")
+                for idx, instrument in enumerate(siglent_list):
+                    print("ID:{0} (Oscilloscope Serial No. {1})".format(str(idx),instrument.split('::')[3]))
+                print("")
+                print("Connecting to oscilloscope (ID {0}).".format(str(instrument_id)))
+                self.osc = self.rm.open_resource(siglent_list[instrument_id])
+                print("")
+                print("Connected.")
+                return
+            except:
+                print("Couldn't connect to selected oscilloscope.")
         else:
             print("No Siglent oscilloscopes found, or you've selected a nonexistent oscilloscope ID.")
             return
@@ -52,17 +54,29 @@ class Oscilloscope(object):
         print(Oscilloscope.format_results(query_results))
 
     def set_vdiv(self, channel, value):
-        """accepted values: 2mV, 5mV, 10mV, 50mV, 100mV, 200mV, 500mV, 1V, 2V, 5V, (also 10V if not CFL series)"""
+        """accepted values: 2mV, 5mV, 10mV, 50mV, 100mV, 200mV, 500mV, 1V, 2V, 5V,
+            (also 10V if not CFL series)"""
         if value in ('2mV', '5mV', '10mV', '50mV', '100mV', '200mV', '500mV', '1V', '2V', '5V', '10V'):
-            self.osc.write('C{0}:VDIV {1}'.format(str(channel), value.upper()))
+            try:
+                self.osc.write('C{0}:VDIV {1}'.format(str(channel), value.upper()))
+                print("Success. Voltage division set to {0}.".format(value))
+            except:
+                print("Failed to set specified voltage division.")
         else:
             print("Invalid voltage division value.")
 
     def set_tdiv(self, channel, value):
-        """accepted values: 5ns, 10ns, 25ns, 50ns, 100ns, 250ns, 500ns, 1us, 2.5us, 5us, 10us, 25us, 50us, 
-        100us, 250us, 500us, 1ms, 2.5ms, 5ms, 10ms, 25ms, 50ms, 100ms, 250ms, 500ms, 1s, 2.5s, 5s, 10s, 25s, 50s"""# 1ns and 2.5ns can be added (applicable for certain models of oscilloscope)
-        if value in (5ns, 10ns, 25ns, 50ns, 100ns, 250ns, 500ns, 1us, 2.5us, 5us, 10us, 25us, 50us, 100us, 250us, 500us, 1ms, 2.5ms, 5ms, 10ms, 25ms, 50ms, 100ms, 250ms, 500ms, 1s, 2.5s, 5s, 10s, 25s, 50s):
-            self.osc.write('C{0}:TDIV {1}'.format(str(channel), value.upper()))
+        """accepted values: 5ns, 10ns, 25ns, 50ns, 100ns, 250ns, 500ns, 1us, 2.5us, 5us,
+           10us, 25us, 50us, 100us, 250us, 500us, 1ms, 2.5ms, 5ms, 10ms, 25ms, 50ms, 100ms,
+           250ms, 500ms, 1s, 2.5s, 5s, 10s, 25s, 50s"""
+        if value in ('5ns', '10ns', '25ns', '50ns', '100ns', '250ns', '500ns', '1us', '2.5us', '5us', '10us', '25us',
+                     '50us', '100us', '250us', '500us', '1ms', '2.5ms', '5ms', '10ms', '25ms', '50ms', '100ms', '250ms',
+                     '500ms', '1s', '2.5s', '5s', '10s', '25s', '50s'):# 1ns and 2.5ns can be added (applicable for certain models of oscilloscope)
+            try:
+                self.osc.write('C{0}:TDIV {1}'.format(str(channel), value.upper()))
+                print("Success. Time division set to {0}.".format(value))
+            except:
+                print("Failed to set specified time division.")
         else:
             print("Invalid time division value.")
 
