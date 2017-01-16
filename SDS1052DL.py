14b0e11502dd0d9bfbc9ce1b96f9736a5add26a8
jgerschler <jjgerschler@gmail.com>
2017-01-15 18:32:23 -0600

    in progress -- not useful yet

--- SDS1052DL.py
+++ SDS1052DL.py
@@ -3,9 +3,9 @@
 import visa, time
 
 class Oscilloscope(object):
-    delay = 0.01
-    #def __init__(self):
-
+    def __init__(self):
+        self.delay = 0.01
+        
     def connect(self, instrument_id=0):
         self.rm = visa.ResourceManager()
         siglent_list = [instrument for instrument in self.rm.list_resources() if instrument.split('::')[1] == '0xF4EC']
@@ -14,24 +14,43 @@ class Oscilloscope(object):
             for idx, instrument in enumerate(siglent_list):
                 print("ID:{0} (Oscilloscope Serial No. {1})".format(str(idx),instrument.split('::')[3]))
             print("")
-            print("Connecting to oscilloscope (ID {}).".format(str(instrument_id)))
+            print("Connecting to oscilloscope (ID {0}).".format(str(instrument_id)))
             self.osc = self.rm.open_resource(siglent_list[instrument_id])
             print("")
             print("Connected.")
             return
         else:
-            print("No Siglent oscilloscopes found, or you've selected a nonexistent oscilloscope ID")
+            print("No Siglent oscilloscopes found, or you've selected a nonexistent oscilloscope ID.")
             return
             
     def disconnect(self):
         self.rm.close()
-        
+    
     def measure_vpp(self, channel):
-        query_results = self.osc.query('C{}:PAVA? PKPK'.format(str(channel)))
-        time.sleep(Oscilloscope.delay)
-        print(query_results)
+        query_results = self.osc.query('C{0}:PAVA? PKPK'.format(str(channel)))
+        time.sleep(self.delay)
+        print(Oscilloscope.format_results(query_results))
         
-##    def measure_vpp(self, channel):
-##        query_results = self.osc.query('C{}:PAVA? PKPK'.format(str(channel)))
-##        time.sleep(Oscilloscope.delay)
-##        print(query_results)
+    def measure_vmax(self, channel):
+        query_results = self.osc.query('C{0}:PAVA? MAX'.format(str(channel)))
+        time.sleep(self.delay)
+        print(Oscilloscope.format_results(query_results))
+
+    def measure_rms(self, channel):
+        query_results = self.osc.query('C{0}:PAVA? RMS'.format(str(channel)))
+        time.sleep(self.delay)
+        print(Oscilloscope.format_results(query_results))
+
+    def measure_freq(self, channel):
+        query_results = self.osc.query('C{0}:PAVA? FREQ'.format(str(channel)))
+        time.sleep(self.delay)
+        print(Oscilloscope.format_results(query_results))
+
+    def measure_period(self, channel):
+        query_results = self.osc.query('C{0}:PAVA? PER'.format(str(channel)))
+        time.sleep(self.delay)
+        print(Oscilloscope.format_results(query_results))
+
+    @staticmethod# fix this
+    def format_results(query_results):
+        return query_results.split(',')[1]
